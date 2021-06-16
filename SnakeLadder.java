cc3b91cd2daff969825fe7ab371e5b31b8d23d6b
pranav21999 <pranavdani99@gmail.com>
2021-06-17 00:53:22 +0530

    Welcome to Snake And Ladder Game

---
+++ SnakeLadder.java
@@ -0,0 +1,12 @@
+public class SnakeLadder
+{
+	void welcome()
+	{
+		System.out.println("welcome to Snake and Ladder problem.");
+	}
+	public static void main(String args[])
+	{
+		SnakeLadder s=new SnakeLadder();
+		s.welcome();
+	}
+}
